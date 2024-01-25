4ad531e030eaf2116611610ef45e9f3f5c914c5c
caoramires <112504913+caoramires@users.noreply.github.com>
2024-01-25 19:51:54 -0300

    Inclusão inicial
    Primeira inclusão de arquivos

---
+++ bannervendedor.py
@@ -0,0 +1,50 @@
+from botoes import ImageButton, LabelButton
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.graphics import Color, Rectangle
+import requests
+from kivy.app import App
+from functools import partial
+
+class BannerVendedor(FloatLayout):
+
+    def __init__(self, **kwargs):
+        super().__init__()
+
+        with self.canvas:
+            Color(rgb=(0, 0, 0, 1))
+            self.rec = Rectangle(size=self.size, pos=self.pos)
+        self.bind(pos=self.atualizar_rec, size=self.atualizar_rec)
+
+        id_vendedor = kwargs["id_vendedor"]
+
+          # obriga Regras na DB "indexOn": ["id_vendedor"]
+
+        link = f'https://aplicativovendashash-39c91-default-rtdb.firebaseio.com/.json?orderBy="id_vendedor"&equalTo="{id_vendedor}"'
+        requisicao = requests.get(link)
+        requisicao_dic = requisicao.json()
+            # retira somente o dicionário de valores do requisicao_dic que é uma lista de dicionarios
+        valor = list(requisicao_dic.values())[0]
+        avatar = valor['avatar']
+        total_vendas = valor['total_vendas']
+
+        meu_aplicativo = App.get_running_app()
+
+        imagem = ImageButton(source=f"icones/fotos_perfil/{avatar}",
+                             pos_hint={"right":0.4, "top":0.9}, size_hint=(0.3, 0.8),
+                             on_release=partial(meu_aplicativo.carregar_vendas_vendedor, valor) )
+        label_id = LabelButton(text=f"ID Vendedor: {id_vendedor}",
+                               pos_hint={"right":0.9, "top":0.9}, size_hint=(0.5, 0.5),
+                               on_release=partial(meu_aplicativo.carregar_vendas_vendedor, valor))
+        label_total = LabelButton(text=f"Total de Vendas: R$ {total_vendas}",
+                                  pos_hint={"right":0.9, "top":0.6}, size_hint=(0.5, 0.5),
+                                  on_release=partial(meu_aplicativo.carregar_vendas_vendedor, valor))
+
+        self.add_widget(imagem)
+        self.add_widget(label_id)
+        self.add_widget(label_total)
+
+        # função que atualiza o fundo
+    def atualizar_rec(self, *args):
+        self.rec.pos = self.pos
+        self.rec.size = self.size
